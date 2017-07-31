eda4c6652201f00982e6453aee115ce3e0603057
kavinkumar06 <kavinm09@gmail.com>
2017-07-31 14:26:48 +0530

    Create Ruby.java

---
+++ Ruby.java
@@ -0,0 +1,22 @@
+# Ruby
+import java.io.*;
+import java.util.*;
+public class Ruby
+{
+  public static void main(String args[])
+  {
+    String s1="hello";
+	      for( int i=0;i<=s1.length();i++)
+	      {
+	         char e=s1.charAt(i);
+	         if((e== 'A')||(e=='E')||(e=='I')||(e=='O')||(e=='U'))
+	         {
+	             String h=s1.substring(0, i);
+	             String g=s1.substring(i+ i);
+	             s1=h+"Z"+g;
+	         }
+	     }   
+	    System.out.println("The result is="+s1);	                
+    }
+
+}
